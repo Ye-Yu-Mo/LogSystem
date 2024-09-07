34aba4bbe8e792d05925b762186d00e505f8b843
XL <2908156919@qq.com>
2024-09-07 08:10:00 +0800

    实现了日志等级类

---
+++ level.hpp
@@ -0,0 +1,49 @@
+#pragma once
+/*
+    日志等级类的实现
+        1. 定义枚举类，枚举出日志的等级
+        2. 提供转换接口，将枚举转换成对应的字符串
+*/
+
+namespace Xulog
+{
+    class LogLevel
+    {
+    public:
+        enum class value
+        {
+            UNKNOW = 0,
+            DEBUG,
+            INFO,
+            WARN,
+            ERROR,
+            FATAL,
+            OFF
+        };
+        static const char *toString(LogLevel::value level)
+        {
+            switch (level)
+            {
+            case LogLevel::value::DEBUG:
+                return "DEBUG";
+                break;
+            case LogLevel::value::INFO:
+                return "INFO";
+                break;
+            case LogLevel::value::WARN:
+                return "WARN";
+                break;
+            case LogLevel::value::ERROR:
+                return "ERROR";
+                break;
+            case LogLevel::value::FATAL:
+                return "FATAL";
+                break;
+            case LogLevel::value::OFF:
+                return "OFF";
+                break;
+            }
+            return "UNKNOW";
+        }
+    };
+}
--- test.cc
+++ test.cc
@@ -1,10 +1,19 @@
 #include "util.hpp"
-
+#include "level.hpp"
 int main()
 {
-    std::cout << Xulog::Util::Date::getTime() << std::endl;
-    std::string pathname = "./dir1/dir2/";
-    Xulog::Util::File::createDirectory(Xulog::Util::File::path(pathname));
+    // 测试工具类
+    // std::cout << Xulog::Util::Date::getTime() << std::endl;
+    // std::string pathname = "./dir1/dir2/";
+    // Xulog::Util::File::createDirectory(Xulog::Util::File::path(pathname));
 
+    // 测试等级类
+    std::cout << Xulog::LogLevel::toString(Xulog::LogLevel::value::DEBUG) << std::endl;
+    std::cout << Xulog::LogLevel::toString(Xulog::LogLevel::value::INFO) << std::endl;
+    std::cout << Xulog::LogLevel::toString(Xulog::LogLevel::value::WARN) << std::endl;
+    std::cout << Xulog::LogLevel::toString(Xulog::LogLevel::value::ERROR) << std::endl;
+    std::cout << Xulog::LogLevel::toString(Xulog::LogLevel::value::FATAL) << std::endl;
+    std::cout << Xulog::LogLevel::toString(Xulog::LogLevel::value::OFF) << std::endl;
+    std::cout << Xulog::LogLevel::toString(Xulog::LogLevel::value::UNKNOW) << std::endl;
     return 0;
 }
