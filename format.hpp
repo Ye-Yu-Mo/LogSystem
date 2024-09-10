440d4b5aa9d23bc66c68d89e8342b49ae9825360
XL <2908156919@qq.com>
2024-09-10 12:50:36 +0800

    添加了格式化器的智能指针

--- format.hpp
+++ format.hpp
@@ -140,6 +140,7 @@ namespace Xulog
     class Formatter
     {
     public:
+        using ptr = std::shared_ptr<Formatter>;
         Formatter(const std::string &pattern = "[%d{%y-%m-%d|%H:%M:%S}][%t][%c][%f:%l][%p]%T%m%n")
             : _pattern(pattern)
         {
